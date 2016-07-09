49ce987b1c2f9c04798866353e1bc2d2a881e0f9
Christopher Khoo <khooweiwen@gmail.com>
2016-07-09 15:16:08 +0800

    questions bank, player names

--- main.js
+++ main.js
@@ -2,150 +2,257 @@ $(document).ready(function() {
   console.log('JS linked');
 
   var deckArray = [{
-    question: 'q1',
-    optionA: 'q1oA',
-    optionB: 'q1oB',
-    optionC: 'q1oC',
-    optionD: 'q1oD',
+    question: "What is the name of Tin tin's dog?",
+    optionA: 'Snowflake',
+    optionB: 'Whitey',
+    optionC: 'Snowy',
+    optionD: 'Josh',
+    answer: 'C'
+  }, {
+    question: "What is the name of Hagrid's dog in Harry Potter?",
+    optionA: 'Fang',
+    optionB: 'Crookshanks',
+    optionC: 'Hedwig',
+    optionD: 'Fawkes',
+    answer: 'A'
+  }, {
+    question: "What is the name of the Doctor's dog (from Dr Who)?" ,
+    optionA: 'Ajax',
+    optionB: 'K-9',
+    optionC: 'Codex',
+    optionD: 'Digit',
     answer: 'B'
   }, {
-    question: 'q2',
-    optionA: 'q2oA',
-    optionB: 'q2oB',
-    optionC: 'q2oC',
-    optionD: 'q2oD',
+    question: "What is the name of Bill Clinton's dog?",
+    optionA: 'Rollo',
+    optionB: 'Fido',
+    optionC: 'Jet',
+    optionD: 'Buddy',
+    answer: 'D'
+  }, {
+    question: "What is the name of Odysseus's faithful hound in Homer's Odyssey?",
+    optionA: 'Argos',
+    optionB: 'Ace',
+    optionC: 'Castor',
+    optionD: 'Rex',
+    answer: 'A'
+  }, {
+    question: "What is the name of Lord Byron's dog?",
+    optionA: 'Rex',
+    optionB: 'Boatswain',
+    optionC: 'Thespis',
+    optionD: 'Valerian',
     answer: 'B'
   }, {
-    question: 'q3',
-    optionA: 'q3oA',
-    optionB: 'q3oB',
-    optionC: 'q3oC',
-    optionD: 'q3oD',
+    question: "What is the name of the bulldog in Tom and Jerry cartoons?",
+    optionA: 'Bud',
+    optionB: 'Crusher',
+    optionC: 'Punch',
+    optionD: 'Spike',
+    answer: 'D'
+  }, {
+    question: "What is the name of the First Dog of the United States? (Obama's first)",
+    optionA: 'Duxton',
+    optionB: 'Sunny',
+    optionC: 'Bo',
+    optionD: 'Buddy',
+    answer: 'C'
+  }, {
+    question: "President Barack Obama's two dogs are of which breed?",
+    optionA: 'American Cocker Spaniel',
+    optionB: 'Tibetan Mastiff',
+    optionC: 'Golden Retriever',
+    optionD: 'Portuguese Water Dog',
+    answer: 'D'
+  }, {
+    question: "What is the name of President Barack Obama's second dog?",
+    optionA: 'Sunny',
+    optionB: 'Bo',
+    optionC: 'Dash',
+    optionD: 'Jet',
+    answer: 'A'
+  }, {
+    question: 'What breed of dog is Queen Elizabeth II strongly associated with?',
+    optionA: 'British Bulldog',
+    optionB: 'Corgis',
+    optionC: 'Scottish Terrier',
+    optionD: 'West Highland White Terrier',
     answer: 'B'
   }, {
-    question: 'q4',
-    optionA: 'q4oA',
-    optionB: 'q4oB',
-    optionC: 'q4oC',
-    optionD: 'q4oD',
+    question: 'What breed is Lassie?',
+    optionA: 'Lassie',
+    optionB: 'St. Bernard',
+    optionC: 'Collie',
+    optionD: 'Old English Sheepdog',
+    answer: 'C'
+  }, {
+    question: 'What breed is Snoopy from Peasnuts?',
+    optionA: 'Border Collie',
+    optionB: 'Dachshund',
+    optionC: 'Jack Russell Terrier',
+    optionD: 'Beagle',
+    answer: 'D'
+  }, {
+    question: "What is the name of Stanley Ipkiss's the dog from the Mask?",
+    optionA: 'Kiss',
+    optionB: 'Horlicks',
+    optionC: 'Jack',
+    optionD: 'Milo',
+    answer: 'D'
+  }, {
+    question: 'What breed are the dogs from the movie Beethoven?',
+    optionA: 'St. Bernard',
+    optionB: 'German Shepherd',
+    optionC: 'Doberman Pinscher',
+    optionD: 'Collie',
+    answer: 'A'
+  }, {
+    question: 'What breed of dog is gromit from Wallace and Gromit?',
+    optionA: 'Jack Russell Terrier',
+    optionB: 'Beagle',
+    optionC: 'Golden Retriever',
+    optionD: 'Shar Pei',
     answer: 'B'
   }, {
-    question: 'q5',
-    optionA: 'q5oA',
-    optionB: 'q5oB',
-    optionC: 'q5oC',
-    optionD: 'q5oD',
+    question: "What is the name of Andy's pet dog from the movie Toy Story",
+    optionA: 'Buster',
+    optionB: 'Rusty',
+    optionC: 'Butch',
+    optionD: 'Russell',
+    answer: 'A'
+  }, {
+    question: "What is the breed of Andy's pet dog from Toy Story?",
+    optionA: 'Golden Retriever',
+    optionB: 'Rottweiler',
+    optionC: 'Dachshund',
+    optionD: 'Havanese',
+    answer: 'C'
+  }, {
+    question: "What is the name of The Phantom's dog in the movie serial(based off the comic)?",
+    optionA: 'Devil',
+    optionB: 'Shadow',
+    optionC: 'Dagger',
+    optionD: 'Hound',
+    answer: 'A'
+  }, {
+    question: 'What is the name of the dog from the comic strip Dilbert?',
+    optionA: 'Cuffburgh',
+    optionB: 'Dogbert',
+    optionC: 'Albert',
+    optionD: 'Robert',
     answer: 'B'
   }, {
-    question: 'q6',
-    optionA: 'q6oA',
-    optionB: 'q6oB',
-    optionC: 'q6oC',
-    optionD: 'q6oD',
+    question: "What is the name of Gru's dog from the cartoon Despicable Me?",
+    optionA: 'Hank',
+    optionB: 'Kyle',
+    optionC: 'Gus',
+    optionD: 'Fluffa',
     answer: 'B'
   }, {
-    question: 'q7',
-    optionA: 'q7oA',
-    optionB: 'q7oB',
-    optionC: 'q7oC',
-    optionD: 'q7oD',
+    question: "What is the Mickey Mouse's dog?",
+    optionA: 'Snoopy',
+    optionB: 'Pooch',
+    optionC: 'Pluto',
+    optionD: 'Spike',
+    answer: 'C'
+  }, {
+    question: "What is the name of the dog from Garfield?",
+    optionA: 'Pluto',
+    optionB: 'Odie',
+    optionC: 'Rocky',
+    optionD: 'Snowy',
+    answer: 'A'
+  }, {
+    question: "What is the name of Scooby-Doo's Nephew?",
+    optionA: 'Spike',
+    optionB: 'Scruffy-Too',
+    optionC: 'Slugger',
+    optionD: 'Scrappy-Doo',
+    answer: 'D'
+  }, {
+    question: "What is the name of Snoopy's older brother?",
+    optionA: 'Spike',
+    optionB: 'Stan',
+    optionC: 'Sparky',
+    optionD: 'Slugger',
+    answer: 'A'
+  }, {
+    question: "What is the name of the American Bulldog from the movie Homeward Bound?",
+    optionA: 'Boxer',
+    optionB: 'Shadow',
+    optionC: 'Chance',
+    optionD: 'Sassy',
+    answer: 'C'
+  }, {
+    question: "What is the name of the Golden Retrieve from the movie Homeward Bound?",
+    optionA: 'Rocky',
+    optionB: 'Shadow',
+    optionC: 'Chance',
+    optionD: 'Sassy',
+    answer: 'A'
+  }, {
+    question: "What is the name of the dog protagonist from Disney's All Dogs Go To Heaven?",
+    optionA: 'Buddy',
+    optionB: 'Hooch',
+    optionC: 'Benji',
+    optionD: 'Charlie',
+    answer: 'D'
+  }, {
+    question: "What is the screen name of Marty Crane's dog from the sitcom Frasier?",
+    optionA: 'Eddie',
+    optionB: 'Barney',
+    optionC: 'Rob',
+    optionD: 'Jack',
+    answer: 'A'
+  }, {
+    question: "What is name of Jon Snow's direwolf from the series Game of Thrones?",
+    optionA: 'Ghost',
+    optionB: 'Grey Wind',
+    optionC: 'Shaggydog',
+    optionD: 'Summer',
+    answer: 'A'
+  }, {
+    question: "What is name of Robb Stark's direwolf from the series Game of Thrones?",
+    optionA: 'Ghost',
+    optionB: 'Grey Wind',
+    optionC: 'Shaggydog',
+    optionD: 'Summer',
     answer: 'B'
   }, {
-    question: 'q8',
-    optionA: 'q8oA',
-    optionB: 'q8oB',
-    optionC: 'q8oC',
-    optionD: 'q8oD',
+    question: "What is name of Rickon Stark's direwolf from the series Game of Thrones?",
+    optionA: 'Ghost',
+    optionB: 'Grey Wind',
+    optionC: 'Shaggydog',
+    optionD: 'Nymeria',
+    answer: 'C'
+  }, {
+    question: "What is name of Arya Stark's direwolf from the series Game of Thrones?",
+    optionA: 'Summer',
+    optionB: 'Lady',
+    optionC: 'Shaggydog',
+    optionD: 'Nymeria',
+    answer: 'D'
+  }, {
+    question: "What is name of Sansa Stark's direwolf from the series Game of Thrones?",
+    optionA: 'Summer',
+    optionB: 'Lady',
+    optionC: 'Shaggydog',
+    optionD: 'Nymeria',
     answer: 'B'
   }, {
-    question: 'q9',
-    optionA: 'q9oA',
-    optionB: 'q9oB',
-    optionC: 'q9oC',
-    optionD: 'q9oD',
-    answer: 'B'
-  }, {
-    question: 'q10',
-    optionA: 'q10oA',
-    optionB: 'q10oB',
-    optionC: 'q10oC',
-    optionD: 'q10oD',
-    answer: 'B'
-  }, {
-    question: 'q11',
-    optionA: 'q11oA',
-    optionB: 'q11oB',
-    optionC: 'q11oC',
-    optionD: 'q11oD',
-    answer: 'B'
-  }, {
-    question: 'q12',
-    optionA: 'q12oA',
-    optionB: 'q12oB',
-    optionC: 'q12oC',
-    optionD: 'q12oD',
-    answer: 'B'
-  }, {
-    question: 'q13',
-    optionA: 'q13oA',
-    optionB: 'q13oB',
-    optionC: 'q13oC',
-    optionD: 'q13oD',
-    answer: 'B'
-  }, {
-    question: 'q14',
-    optionA: 'q14oA',
-    optionB: 'q14oB',
-    optionC: 'q14oC',
-    optionD: 'q14oD',
-    answer: 'B'
-  }, {
-    question: 'q15',
-    optionA: 'q15oA',
-    optionB: 'q15oB',
-    optionC: 'q15oC',
-    optionD: 'q15oD',
-    answer: 'B'
-  }, {
-    question: 'q16',
-    optionA: 'q16oA',
-    optionB: 'q16oB',
-    optionC: 'q16oC',
-    optionD: 'q16oD',
-    answer: 'B'
-  }, {
-    question: 'q17',
-    optionA: 'q17oA',
-    optionB: 'q17oB',
-    optionC: 'q17oC',
-    optionD: 'q17oD',
-    answer: 'B'
-  }, {
-    question: 'q18',
-    optionA: 'q18oA',
-    optionB: 'q18oB',
-    optionC: 'q18oC',
-    optionD: 'q18oD',
-    answer: 'B'
-  }, {
-    question: 'q19',
-    optionA: 'q19oA',
-    optionB: 'q19oB',
-    optionC: 'q19oC',
-    optionD: 'q19oD',
-    answer: 'B'
-  }, {
-    question: 'q20',
-    optionA: 'q20oA',
-    optionB: 'q20oB',
-    optionC: 'q20oC',
-    optionD: 'q20oD',
-    answer: 'B'
+    question: "What is name of Bran Stark's direwolf from the series Game of Thrones?",
+    optionA: 'Summer',
+    optionB: 'Lady',
+    optionC: 'Shaggydog',
+    optionD: 'Nymeria',
+    answer: 'A'
   }];
 
   var remainingCards = null;
   var playerScore = [0, 0, 0];
   var currentPlayer = 1;
+  var player1Name = prompt("Player 1's name");
+  var player2Name = prompt("Player 2's name");
   var currentRound = 1;
   var response = null;
   var randomCard = null;
@@ -154,7 +261,15 @@ $(document).ready(function() {
   var randomCardNum = null;
 
   function displaySetup() {
-    $('#currentPlayer').html(currentPlayer);
+    if(currentPlayer==1){
+      $('#currentPlayer').html(player1Name);
+    }
+    else{
+      $('#currentPlayer').html(player2Name);
+    }
+
+    $('#player1Name').html(player1Name);
+    $('#player2Name').html(player2Name);
     $('#player1Score').html(playerScore[1]);
     $('#player2Score').html(playerScore[2]);
     $('#currentRound').html(currentRound);
@@ -163,6 +278,7 @@ $(document).ready(function() {
 
   var pullRandomCard = function(deckArray) {
     remainingCards = deckArray.length;
+    console.log(remainingCards);
     randomCardNum = Math.floor(remainingCards * Math.random());
     randomCard = deckArray[randomCardNum];
     $('#questionDisplay').html(randomCard.question);
@@ -220,8 +336,6 @@ $(document).ready(function() {
     else {
       deckArray.splice(randomCardNum,1);
       playedCards.push(randomCard);
-      console.log("played"+playedCards);
-      console.log("original deck"+deckArray);
       displaySetup();
       pullRandomCard(deckArray);
     }
